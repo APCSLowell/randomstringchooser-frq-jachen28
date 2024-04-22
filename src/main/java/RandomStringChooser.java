fc295424a86500504b85f3e38723a0e727b98476
jachen28 <142927320+jachen28@users.noreply.github.com>
2024-04-21 21:32:17 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,20 +1,21 @@
 import java.util.*;
 public RandomStringChooser{
 private String[] stringChooser;
-private String[] availability;
+private boolean[] availability;
 
 public RandomStringChooser(String [] wordArray){
 availability = new String[wordArray.length];
 stringChooser = wordArray;
 for(int i = 0; i < availability.length; i++)
-availability  = "available";
+availability[i]  = true;
 }
 
 public String getNext(){
 int randomIndex = (int)(Math.random() * availability.length)
-if(availability[randomIndex].equals("available"){
-availability[randomIndex] = "false";
+if(availability[randomIndex] == true){
+availability[randomIndex] = false;
 return stringChooser[randomIndex];
+}
 else {
 return "NONE";
 }

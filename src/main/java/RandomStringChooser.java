f5799263a806c8a17d9616dc879c5f6ae786a09a
jachen28 <142927320+jachen28@users.noreply.github.com>
2024-04-21 22:57:27 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -1,23 +1,22 @@
 import java.util.*;
-public class RandomStringChooser{
-private String[] stringChooser;
-private boolean[] availability;
-    
-public RandomStringChooser(String [] wordArray){
-availability = new boolean[wordArray.length];
-stringChooser = wordArray;
-for(int i = 0; i < availability.length; i++)
-availability[i]  = true;
-}
 
-public String getNext(){
-int randomIndex = (int)(Math.random() * availability.length);
-if(availability[randomIndex] == true){
-availability[randomIndex] = false;
-return stringChooser[randomIndex];
-}
-else {
-return "NONE";
-}
-}
+public class RandomStringChooser {
+    private ArrayList<String> availableStrings;
+
+    public RandomStringChooser(String[] wordArray) {
+        availableStrings = new ArrayList<>();
+        for (int i = 0; i < wordArray.length; i++) {
+            availableStrings.add(wordArray[i]);
+        }
+    }
+
+    public String getNext() {
+        if (availableStrings.isEmpty()) {
+            return "NONE";
+        }
+        int randomIndex = (int)(Math.random() * availableStrings.size());
+        String chosenString = availableStrings.get(randomIndex);
+        availableStrings.remove(randomIndex);
+        return chosenString;
+    }
 }
